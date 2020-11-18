9cb86cecd811ce5d89b6e784fa07e4133dc43228
Scott Upman <scottupman1@gmail.com>
2020-11-17 20:56:32 -0500

    Upload

---
+++ Scripts/DisplaySelectedButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class DisplaySelectedButton : MonoBehaviour, ISelectHandler, IDeselectHandler
+{
+    public GameObject trianglePNG;
+
+    void Start()
+    {
+        trianglePNG.SetActive(false);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        trianglePNG.SetActive(true);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        trianglePNG.SetActive(false);
+    }
+    
+}
---
+++ Scripts/WeaponData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "weaponObject", menuName = "Weapon")]
+public class WeaponData : ScriptableObject
+{
+    [Header("Weapon Stats")]
+    public string weapon;
+    public string atkDamage;
+    public string hitAmount;
+    public string critChance;
+
+    [Header("Weapon Description")]
+    [TextArea]
+    public string weaponDescription;
+
+    public void displayWeaponData()
+    {
+
+    }
+}
